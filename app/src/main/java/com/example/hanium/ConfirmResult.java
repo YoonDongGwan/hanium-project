4eae446b3a961c6d9d4ac04e52226b4b6004c95a
hgfdsa4320 <hgfdsa4320@naver.com>
2021-08-24 15:16:20 +0900

    회원가입 레트로핏 구현 및 이메일 인증 삭제

---
+++ app/src/main/java/com/example/hanium/ConfirmResult.java
@@ -0,0 +1,34 @@
+package com.example.hanium;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ConfirmResult {
+    @SerializedName("success")
+    public String success;
+
+    @SerializedName("message")
+    public String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+
+    public ConfirmResult(String success, String message){
+        this.success = success;
+        this.message = message;
+    }
+}
+
+
--- app/src/main/java/com/example/hanium/RetrofitAPI.java
+++ app/src/main/java/com/example/hanium/RetrofitAPI.java
@@ -14,4 +14,19 @@ public interface RetrofitAPI {
     @FormUrlEncoded
     @POST("auth/temp")
     public Call<LoginResult> find(@Field("email") String email, @Field("phoneNumber") String phoneNumber);
+
+    @FormUrlEncoded
+    @POST("auth/send")
+    public Call<NumberResult> send(@Field("email") String email);
+
+    @FormUrlEncoded
+    @POST("auth/confirm")
+    public Call<ConfirmResult> confirm(@Field("secret") int secret);
+
+    @FormUrlEncoded
+    @POST("auth/signup")
+    public Call<ConfirmResult> signup(@Field("email") String email, @Field("nickname") String nickname,
+                                       @Field("name") String name, @Field("password") String password,
+                                       @Field("confirm_pwd") String confirm_pwd, @Field("phoneNumber") String phoneNumber);
+
 }
